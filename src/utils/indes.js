8c73b5b4c521673086bf2a3b2ec4af726b081369
sabbir-sabbir <iosabbir7890@gmail.com>
2025-08-18 23:11:54 +0600

    hello

--- src/utils/indes.js
+++ src/utils/indes.js
@@ -1,27 +1,39 @@
-export const dateDifferenceFromNow = (startedDate)=> {
-    let difference = new Date().getTime() - new Date(startedDate).getTime();
+export const dateDifferenceFromNow = (startedDate) => {
+  let difference = new Date().getTime() - new Date(startedDate).getTime();
 
+  difference = difference / 1000; // total seconds
+  let dayDifference = Math.floor(difference / 86400); // 1 day = 86400 sec
+  difference -= dayDifference * 86400;
 
-    difference = difference / 1000;
-    let hourDifference = Math.floor(difference / 3600);
-    difference -= hourDifference * 3600;
-    let minuteDifference = Math.floor(difference / 60);
-    difference -= minuteDifference * 60;
+  let hourDifference = Math.floor(difference / 3600);
+  difference -= hourDifference * 3600;
 
-    let message;
+  let minuteDifference = Math.floor(difference / 60);
+  difference -= minuteDifference * 60;
 
-    if(hourDifference > 0){
-        message = `${hourDifference} hour`;
-    }
+  let message;
 
-    if(minuteDifference > 0){
-        message = message ? `${message} ${minuteDifference} minutes` : `${minuteDifference} minutes`
-    }
+  if (dayDifference > 0) {
+    message = `${dayDifference} day${dayDifference > 1 ? "s" : ""}`;
+  }
 
-    if(difference > 0){
-        message = message ? `${message} ${Math.round(difference)} seconds` : `${Math.round(difference)} minutes`
-    }
+  if (hourDifference > 0) {
+    message = message
+      ? `${message} ${hourDifference} hour${hourDifference > 1 ? "s" : ""}`
+      : `${hourDifference} hour${hourDifference > 1 ? "s" : ""}`;
+  }
 
-    return message;
+  if (minuteDifference > 0) {
+    message = message
+      ? `${message} ${minuteDifference} minute${minuteDifference > 1 ? "s" : ""}`
+      : `${minuteDifference} minute${minuteDifference > 1 ? "s" : ""}`;
+  }
 
-}
+  if (difference > 0) {
+    message = message
+      ? `${message} ${Math.round(difference)} second${Math.round(difference) > 1 ? "s" : ""}`
+      : `${Math.round(difference)} second${Math.round(difference) > 1 ? "s" : ""}`;
+  }
+
+  return message || "0 seconds";
+};
